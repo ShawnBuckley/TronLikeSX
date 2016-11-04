ef672315f3fef68994130f44ed0885e6c97fd9a3
Shawn Buckley <ShawnBuckley@users.noreply.github.com>
2016-11-04 19:45:43 -0400

    Move map prep to GameMap

--- src/main/scala/tronlikesx/client/Client.scala
+++ src/main/scala/tronlikesx/client/Client.scala
@@ -2,8 +2,7 @@ package tronlikesx.client
 
 import org.scalajs.dom.document
 import org.scalajs.dom.raw.{HTMLCanvasElement, HTMLImageElement}
-import tronlikesx.common.display.DisplayObject
-import tronlikesx.common.map.{GameMap, MapTile, Terrain, TerrainFlags}
+import tronlikesx.common.map.GameMap
 
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
@@ -11,39 +10,9 @@ import scala.scalajs.js.annotation.JSExport
 @JSExport("Client")
 object Client extends JSApp {
 
-  def prepMap(width: Int, height: Int): GameMap = {
-    val map = new GameMap(width, height)
-
-    val solidRender = TerrainFlags(render = true, solid = true)
-    val wallDisplay = DisplayObject('#', "#ffffff")
-    val wall = Terrain(solidRender, wallDisplay)
-
-    map.tiles(0)(0) = MapTile(wall)
-    map.tiles(width-1)(height-1) = MapTile(wall)
-
-    for(i <- 1 until width) {
-      map.tiles(i)(0) = MapTile(wall)
-      map.tiles(i)(height-1) = MapTile(wall)
-    }
-
-    for(i <- 1 until height) {
-      map.tiles(0)(i) = MapTile(wall)
-      map.tiles(width-1)(i) = MapTile(wall)
-    }
-
-    val floorDisplay = DisplayObject('.', "#ffffff")
-    val floor = Terrain(TerrainFlags(render = true, solid = false), floorDisplay)
-
-    for(x <- 1 until width-1) {
-      for(y <- 1 until height-1) {
-        map.tiles(x)(y) = MapTile(floor)
-      }
-    }
-    map
-  }
-
   def main(): Unit = {
-    val map = prepMap(10, 15)
+    val map = new GameMap(10,15)
+    map.init()
 
     val sprite = document.getElementById("sprite").asInstanceOf[HTMLImageElement]
     val canvas = document.getElementById("canvas").asInstanceOf[HTMLCanvasElement]
@@ -55,7 +24,8 @@ object Client extends JSApp {
   }
 
   def main(args: Array[String]) = {
-    val map = prepMap(10, 15)
+    val map = new GameMap(10,15)
+    map.init()
 
     println(s"${map.width} ${map.height}")
 
--- src/main/scala/tronlikesx/common/map/GameMap.scala
+++ src/main/scala/tronlikesx/common/map/GameMap.scala
@@ -1,5 +1,7 @@
 package tronlikesx.common.map
 
+import tronlikesx.common.display.DisplayObject
+
 import scala.collection.mutable
 
 case class GameMap(width: Int, height: Int) {
@@ -11,6 +13,34 @@ case class GameMap(width: Int, height: Int) {
       row.insert(y, null) // new MapTile(null)
     }
     tiles.insert(x, row)
+  }
+
+  def init() = {
+    val solidRender = TerrainFlags(render = true, solid = true)
+    val wallDisplay = DisplayObject('#', "#ffffff")
+    val wall = Terrain(solidRender, wallDisplay)
+
+    tiles(0)(0) = MapTile(wall)
+    tiles(width-1)(height-1) = MapTile(wall)
+
+    for(i <- 1 until width) {
+      tiles(i)(0) = MapTile(wall)
+      tiles(i)(height-1) = MapTile(wall)
+    }
+
+    for(i <- 1 until height) {
+      tiles(0)(i) = MapTile(wall)
+      tiles(width-1)(i) = MapTile(wall)
+    }
+
+    val floorDisplay = DisplayObject('.', "#ffffff")
+    val floor = Terrain(TerrainFlags(render = true, solid = false), floorDisplay)
+
+    for(x <- 1 until width-1) {
+      for(y <- 1 until height-1) {
+        tiles(x)(y) = MapTile(floor)
+      }
+    }
   }
 }
 
