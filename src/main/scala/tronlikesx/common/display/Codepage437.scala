d26f8bce5a11ead8cab84552ecbed627cb002a2c
Shawn Buckley <ShawnBuckley@users.noreply.github.com>
2016-11-07 00:01:19 -0500

    Add Codepage437 definitions and comments

--- src/main/scala/tronlikesx/common/display/Codepage437.scala
+++ src/main/scala/tronlikesx/common/display/Codepage437.scala
@@ -1,16 +1,20 @@
 package tronlikesx.common.display
 
 object Codepage437 {
-  val triangle_right: Char = 16
-  val triangle_left: Char = 17
-  val triangle_up: Char = 30
-  val triangle_down: Char = 31
-  val space: Char = 32
-  val medium_shade: Char = 177
-  val vertical_line:Char = 179
-  val horizontal_line: Char = 196
-  val cross: Char = 197
-  val full_block: Char = 219
-  val interpunct: Char = 250
-  val square: Char = 254
+  val triangle_right: Char = 16           // ►
+  val triangle_left: Char = 17            // ◄
+  val triangle_up: Char = 30              // ▲
+  val triangle_down: Char = 31            // ▼
+  val space: Char = 32                    //
+  val medium_shade: Char = 177            // ▒
+  val vertical_line:Char = 179            // │
+  val single_down_and_left: Char = 191    // ┐
+  val single_up_and_right: Char = 192     // └
+  val horizontal_line: Char = 196         // ─
+  val single_up_and_left: Char = 217      // ┘
+  val single_down_and_right: Char = 218   // ┌
+  val cross: Char = 197                   // ┼
+  val full_block: Char = 219              // █
+  val interpunct: Char = 250              // ·
+  val square: Char = 254                  // ■
 }
