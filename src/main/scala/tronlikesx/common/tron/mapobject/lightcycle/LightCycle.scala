b81eba2c520ba662e1819cf0e67fe36d4e9593bb
Shawn Buckley <ShawnBuckley@users.noreply.github.com>
2016-11-06 22:14:07 -0500

    Add directional sprite to light cycle

--- src/main/scala/tronlikesx/common/tron/mapobject/lightcycle/LightCycle.scala
+++ src/main/scala/tronlikesx/common/tron/mapobject/lightcycle/LightCycle.scala
@@ -7,18 +7,24 @@ import tronlikesx.common.Game
 import tronlikesx.common.math.Vec2
 
 class LightCycle(color: String, startLocation: Vec2) extends MapObject(new DisplayObject('B', Codepage437.square, color), startLocation, ActionTime(tick = 1000)) with TimeObject {
-  var vector = new Vec2(+1,0)
+  var vector = new Vec2(0,0)
   var energy = 0
 
   Game.session.time.link(this)
 
   override def move(newVec2: Vec2): Boolean = {
     // Disallow ordinal movement
-    if((newVec2.x != 0 && newVec2.y == 0) || (newVec2.x == 0 && newVec2.y != 0)) {
-      display = new DisplayObject(display.print, 'B', display.color)
+    if(((newVec2.x != 0 && newVec2.y == 0) || (newVec2.x == 0 && newVec2.y != 0)) && -newVec2 != vector) {
       vector = newVec2
-      true
-    } else false
+      vector match {
+        case Vec2(-1, 0) => display = new DisplayObject(display.print, Codepage437.triangle_left, display.color)
+        case Vec2( 0, 1) => display = new DisplayObject(display.print, Codepage437.triangle_down, display.color)
+        case Vec2( 0,-1) => display = new DisplayObject(display.print, Codepage437.triangle_up, display.color)
+        case Vec2( 1, 0) => display = new DisplayObject(display.print, Codepage437.triangle_right, display.color)
+      }
+      return true
+    }
+    false
   }
 
   override def tick(time: Int): Unit = {
