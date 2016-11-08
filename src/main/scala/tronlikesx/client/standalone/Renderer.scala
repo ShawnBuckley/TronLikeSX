db5d0545d91a277961522c31703cc1105602fd80
Shawn Buckley <ShawnBuckley@users.noreply.github.com>
2016-11-07 21:20:57 -0500

    Center display

--- src/main/scala/tronlikesx/client/standalone/Renderer.scala
+++ src/main/scala/tronlikesx/client/standalone/Renderer.scala
@@ -11,12 +11,16 @@ class Renderer(sprite: HTMLImageElement, canvas: HTMLCanvasElement) extends comm
     render(x, y, display.render, display.color)
 
   def render(map: Map): Unit = {
+
+    val offset = ((((canvas.width/12)-map.width)/2)+1,
+      (((canvas.height/12)-map.height)/2)+1)
+
     for(x <- 0 until map.width) {
       for(y <- 0 until map.height) {
         map.get(new Vec2(x, y)) match {
           case Some(tile: MapTile) =>
-            render(x, y, tile.terrain.display)
-            tile.mapObjects.foreach(mapObject => render(x, y, mapObject.display))
+            render(x + offset._1, y + offset._2, tile.terrain.display)
+            tile.mapObjects.foreach(mapObject => render(x + offset._1, y + offset._2, mapObject.display))
           case None =>
         }
       }
