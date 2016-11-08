e19c7e0f79ce4fc90f85eba588da3ff95690c410
Shawn Buckley <ShawnBuckley@users.noreply.github.com>
2016-11-07 23:08:47 -0500

    Move web folders into src, map javascript requests to target

--- dev-server.js
+++ dev-server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('.'));
+app.use(express.static('src/main/webapp'));
+
+app.use('/tronlikesx-fastopt.js', express.static('target/scala-2.11/tronlikesx-fastopt.js'));
+app.use('/tronlikesx-fastopt.js.map', express.static('target/scala-2.11/tronlikesx-fastopt.js.map'));
 
 var server = app.listen(3000, function () {
     console.log('Dev server listening at http://%s:%s/', server.address().address, server.address().port);
