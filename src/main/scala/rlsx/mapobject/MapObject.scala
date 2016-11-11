209dc626eb24d135134b1ae50a1aa68d073b7493
Shawn Buckley <ShawnBuckley@users.noreply.github.com>
2016-11-11 10:34:45 -0500

    Use fold for maplinking

--- src/main/scala/rlsx/mapobject/MapObject.scala
+++ src/main/scala/rlsx/mapobject/MapObject.scala
@@ -14,21 +14,11 @@ class MapObject(var display: DisplayObject, var speed: ActionTime, val flags: Fl
   def location =
     _location
 
-  private def mapLink() = {
-    Game.session.map.get(_location) match {
-      case Some(tile: MapTile) => tile.link(this)
-      case None =>
-    }
-  }
+  private def mapLink() =
+    Game.session.map.get(_location).fold()(_.link(this))
 
-  private def mapUnlink() = {
-    if(_location != null) {
-      Game.session.map.get(_location) match {
-        case Some(tile: MapTile) => tile.unlink(this)
-        case None =>
-      }
-    }
-  }
+  private def mapUnlink() =
+    if(_location != null) Game.session.map.get(_location).fold()(_.unlink(this))
 
   def location_=(location: Vec2) = {
     mapUnlink()
